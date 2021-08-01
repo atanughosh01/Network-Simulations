2c58aec2b1c7168555e67ba48775f5041d21e826
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2021-08-02 02:34:03 +0530

    added receiverCheckSum.py

---
+++ src/imports/receiverCheckSum.py
@@ -0,0 +1,48 @@
+
+def binary_sum(x1, x2, x3, x4, k, s_cksum):
+    bin_sum = bin(int(x1, 2) + int(x2, 2) + int(s_cksum, 2) +
+                  int(x3, 2) + int(x4, 2) + int(s_cksum, 2))[2:]
+
+    while len(bin_sum) > k:
+        c = len(bin_sum) - k
+        carry = bin_sum[0:c]
+        bin_sum = bin_sum[c:]
+        bin_sum = bin(int(bin_sum, 2) + int(carry, 2))[2:]
+
+    return bin_sum
+
+
+def gen_CheckSum(data, k, s_cksum):
+    b1 = data[0:k]
+    b2 = data[k:2*k]
+    b3 = data[2*k:3*k]
+    b4 = data[3*k:4*k]
+    bin_sum = binary_sum(b1, b2, b3, b4, k, s_cksum)
+    r_cksum = ""
+    for i in bin_sum:
+        if i == '1':
+            r_cksum += '0'
+        else:
+            r_cksum += '1'
+    return r_cksum
+
+
+def main():
+    data = "10000101011000111001010011101101"
+    packet_count = 4
+    packet_length = int(len(data)/packet_count)
+    sender_check_sum = "10000101"
+    receiver_check_sum = gen_CheckSum(data, packet_length, sender_check_sum)
+    data_word = data + receiver_check_sum
+    print("\nData = " + str(data) +
+          "\nLength of sent data = " + str(len(data)) +
+          "\nNumber of packets = " + str(packet_count) +
+          "\nLength of each packet = " + str(packet_length) +
+          "\nGenerated CheckSum = " + str(receiver_check_sum) +
+          "\nLength of CheckSum = " + str(len(receiver_check_sum)) +
+          "\nDataWord = " + str(data_word) +
+          "\nLength of DataWord = " + str(len(data_word)) + "\n")
+
+
+if __name__ == "__main__":
+    main()
