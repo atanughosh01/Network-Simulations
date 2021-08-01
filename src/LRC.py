1ef71a6b36435e63eb16fd80311048bf626c225c
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2021-08-01 16:58:37 +0530

    added CRC.py, LRC.py, test.txt

---
+++ src/LRC.py
@@ -0,0 +1,15 @@
+data_frame = ["11111111", "11110000", "10101010", "10010010"]
+
+
+def LRC(data_frame):
+    count = 0
+    for i in data_frame:
+        pass
+
+
+def main():
+    LRC(data_frame)
+
+
+if __name__ == "__main__":
+    main()
