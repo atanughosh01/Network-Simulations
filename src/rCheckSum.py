e9866e0fdb670b3067eea0f88b25845f3e923310
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2021-08-01 16:58:12 +0530

    added rCheckSum.py as implementation of CheckSum algorithm for receiver process

---
+++ src/rCheckSum.py
@@ -0,0 +1,51 @@
+
+received_data = "10000101011000111001010011101101"
+packet_count = 4
+packet_length = int(len(received_data)/packet_count)
+
+
+def calc_receiver_checkSum(data, k):
+
+    b1 = data[0:k]
+    b2 = data[k:2*k]
+    b3 = data[2*k:3*k]
+    b4 = data[3*k:4*k]
+
+    bin_sum = bin(int(b1, 2) + int(b2, 2) + int(b3, 2) + int(b4, 2))[2:]
+
+    print("Length of received data = " + str(len(data)) +
+          "\nNumber of packets = " + str(packet_count) +
+          "\nLength of each packet = " + str(k) +
+          "\nBinary sum of packets = " + str(bin_sum) +
+          "\nLength of sum of packets = " + str(len(bin_sum)))
+
+    if len(bin_sum) > k:
+        c = len(bin_sum) - k
+        carry = bin_sum[0:c]
+        bin_sum = bin_sum[c:]
+        print("\nCarry = " + str(carry) +
+              ", Initial binary sum = " + str(bin_sum))
+        bin_sum = bin(int(bin_sum, 2) + int(carry, 2))[2:]
+        print("Final binary sum of packets = " + str(bin_sum))
+
+    elif len(bin_sum) < k:
+        c = k - len(bin_sum)
+        bin_sum = '0'*c + bin_sum
+        print("Final binary sum = " + str(bin_sum))
+
+    check_sum = ""
+    for i in bin_sum:
+        if int(i) == 1:
+            check_sum += "0"
+        elif int(i) == 0:
+            check_sum += "1"
+    print("CheckSum = " + str(check_sum))
+    return check_sum
+
+
+def main():
+    calc_receiver_checkSum(received_data, packet_length)
+
+
+if __name__ == "__main__":
+    main()
