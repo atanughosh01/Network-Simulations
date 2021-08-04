9a832ff06d575d20906bf27438206ab0c720273b
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2021-08-04 23:37:44 +0530

    modified src/ChannelProcess.py

--- src/ChannelProcess.py
+++ src/ChannelProcess.py
@@ -20,5 +20,46 @@ def listen_to_sender():
         print("\nException Caught : " + str(e))
         sys.exit()
 
-        while True:
-            pass
+    while True:
+        Sender, address = ChannelSideSocket.accept()
+        ip, port = str(address[0]), str(address[1])
+        print("|Connected to : " + ip + ':' + port)
+        sender_process()
+
+
+def sender_process(connection):
+    connection.send(str.encode("Receiver is Working : "))
+    while True:
+        Sender_request = connection.recv(2048)
+        Receiver_data, choice = [str(i) for i in Sender_request.decode("utf-8").split("\n")]
+        print(Receiver_data)
+        print(choice)
+
+        if choice == '1':
+            vrc_data = vrc.gen_VRC(Receiver_data)
+            if vrc_data == '0':
+                Receiver_response = "Error Checking Complete\nNo Error found in Receiver-data"
+            else:
+                Receiver_response = "Error Checking Complete\nError found in Receiver-data"
+            connection.sendall(str.encode(Receiver_response))
+
+        elif choice == '2':
+            lrc_data = lrc.gen_LRC(Receiver_data)
+            if lrc_data == '0':
+                Receiver_response = "Error Checking Complete\nNo Error found in Receiver-data"
+            else:
+                Receiver_response = "Error Checking Complete\nError found in Receiver-data"
+            connection.sendall(str.encode(Receiver_response))
+
+        elif choice == '3':
+            packet_count = 5
+            packet_length = int(len(Receiver_data)/packet_count)
+            receiver_check_sum = rcs.gen_CheckSum(Receiver_data, packet_length)
+            connection.sendall(str.encode(receiver_check_sum))
+
+        elif choice == '4':
+            key = input("Enter key for calculating CRC \n(Must be same as Sender's Key to get authentic results) : ")
+            crc_data = crc.gen_CRC(Receiver_data, key)
+            Receiver_response = crc_data
+            connection.sendall(str.encode(Receiver_response))
+    # connection.close()
