f7c47bdea10dec01aac2b1e18639ec11d3f07665
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2021-08-06 00:55:04 +0530

    modified src/SenderProcess.py

--- src/SenderProcess.py
+++ src/SenderProcess.py
@@ -1,57 +1,60 @@
 import sys
 import socket
-import packages.CRC as crc
 import packages.VRC as vrc
 import packages.LRC as lrc
+import packages.CRC as crc
 import packages.senderCheckSum as scs
+import packages.receiverCheckSum as rcs
+from _thread import start_new_thread
 
 
-def sendData():
+def send_data():
     host = '127.0.0.1'
-    port = 2021
-    SenderSideSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sender_port = 2021
+    sender_side_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
     try:
-        SenderSideSocket.connect((host, port))
+        sender_side_socket.connect((host, sender_port))
     except socket.error as e:
         print("Channel is Currently Inactive. Connection Failed!\n\nError : " + str(sys.exc_info()))
         print("\nException Caught : " + str(e))
         sys.exit()
 
-    print("Channel is Currently Active.\nSend Request to Get Response Fom Receiver.")
+    print("Channel is Currently Active.\nSend Request to Get Response From Receiver.")
 
-    Receiver_response = SenderSideSocket.recv(1024)
+    receiver_response = sender_side_socket.recv(1024)
 
     while True:
         data = input("\nEnter data to send (Enter 0 to Exit) : ")
         if data == '0':
-            SenderSideSocket.close()
+            sender_side_socket.close()
             print("\nConnection Has Been Closed.\nSender Has Exited.")
             sys.exit()
-        print("Generate Redundant data -> Select 1 for VRC, 2 for LRC, 3 for CheckSum, 4 for CRC ")
 
+        print("Generate Redundant data -> Select 1 for VRC, 2 for LRC, 3 for CheckSum, 4 for CRC ")
         choice = int(input("Enter Your Choice (1, 2, 3 or 4) : "))
+
         if choice == 1:
             vrc_data = vrc.gen_VRC(data)
             sent_data = data + vrc_data
-            SenderSideSocket.sendall(str.encode("\n".join([str(sent_data), str(choice)])))
-            Receiver_response = SenderSideSocket.recv(1024)
-            print(Receiver_response.decode("utf-8"))
+            sender_side_socket.sendall(str.encode("\n".join([str(sent_data), str(choice)])))
+            receiver_response = sender_side_socket.recv(1024)
+            print(receiver_response.decode("utf-8"))
 
         elif choice == 2:
             lrc_data = lrc.gen_LRC(data)
             sent_data = data + lrc_data
-            SenderSideSocket.sendall(str.encode("\n".join([str(sent_data), str(choice)])))
-            Receiver_response = SenderSideSocket.recv(1024)
-            print(Receiver_response.decode("utf-8"))
+            sender_side_socket.sendall(str.encode("\n".join([str(sent_data), str(choice)])))
+            receiver_response = sender_side_socket.recv(1024)
+            print(receiver_response.decode("utf-8"))
 
         elif choice == 3:
             packet_count = 4
             packet_length = int(len(data)/packet_count)
             sender_check_sum = scs.gen_CheckSum(data, packet_length)
             sent_data = data + sender_check_sum
-            SenderSideSocket.sendall(str.encode("\n".join([str(sent_data), str(choice)])))
-            receiver_check_sum = SenderSideSocket.recv(1024).decode("utf-8")
+            sender_side_socket.sendall(str.encode("\n".join([str(sent_data), str(choice)])))
+            receiver_check_sum = sender_side_socket.recv(1024).decode("utf-8")
             if int(receiver_check_sum) == 0:
                 print("Error Checking Complete\nNo Error found in Receiver-data")
             else:
@@ -62,9 +65,9 @@ def sendData():
             key = input("Enter key for calculating CRC : ")
             crc_data = crc.gen_CRC(data, key)
             sent_data = data + crc_data
-            SenderSideSocket.sendall(str.encode("\n".join([str(sent_data), str(choice)])))
-            Receiver_response = SenderSideSocket.recv(1024).decode("utf-8")
-            if int(Receiver_response) == 0:
+            sender_side_socket.sendall(str.encode("\n".join([str(sent_data), str(choice)])))
+            receiver_response = sender_side_socket.recv(1024).decode("utf-8")
+            if int(receiver_response) == 0:
                 print("Error Checking Complete\nNo Error found in Receiver-data")
             else:
                 print("Error Checking Complete\nError found in Receiver-data")
@@ -74,7 +77,7 @@ def sendData():
 
 
 def main():
-    sendData()
+    send_data()
 
 
 if __name__ == "__main__":
