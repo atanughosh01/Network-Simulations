02530fd3b95839fcadb63efedbc1c8edf8217a25
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2021-08-08 04:14:20 +0530

    modified src/ReceiverProcess.py

--- src/ReceiverProcess.py
+++ src/ReceiverProcess.py
@@ -10,7 +10,7 @@ from _thread import start_new_thread
 
 def receive_data():
     host = '127.0.0.1'
-    receiver_port = 2000                                # arbitrary non-privileged port
+    receiver_port = 65432                                # arbitrary non-privileged port
     thread_count = 0
     receiver_side_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -42,7 +42,6 @@ def receive_data():
         thread_count += 1
         print("Thread Number = Sender Number = Channel Number = " + str(thread_count))
 
-    # ReceiverSideSocket.close()
     print(f"\nMore Than {max} Senders/Channels aren't allowed to send request(s).")
     print("Receiver has been closed.")
 
@@ -52,37 +51,42 @@ def channel_thread(connection: socket.socket):
     while True:
         sender_request = connection.recv(2048)
         receiver_data, choice = [str(i) for i in sender_request.decode("utf-8").split("\n")]
-        # print(receiver_data)
-        # print(choice)
 
         if choice == '1':
             vrc_data = vrc.gen_VRC(receiver_data)
             if vrc_data == '0':
-                Receiver_response = "Error Checking Complete\nNo Error found in Receiver-data"
+                receiver_response = "\nError Checking Complete\nNo Error found in Receiver-data"
             else:
-                Receiver_response = "Error Checking Complete\nError found in Receiver-data"
-            connection.sendall(str.encode(Receiver_response))
+                receiver_response = "\nError Checking Complete\nError found in Receiver-data"
+            connection.sendall(str.encode(receiver_response))
 
         elif choice == '2':
             lrc_data = lrc.gen_LRC(receiver_data)
             if lrc_data == '0':
-                Receiver_response = "Error Checking Complete\nNo Error found in Receiver-data"
+                receiver_response = "\nError Checking Complete\nNo Error found in Receiver-data"
             else:
-                Receiver_response = "Error Checking Complete\nError found in Receiver-data"
-            connection.sendall(str.encode(Receiver_response))
+                receiver_response = "\nError Checking Complete\nError found in Receiver-data"
+            connection.sendall(str.encode(receiver_response))
 
         elif choice == '3':
             packet_count = 5
             packet_length = int(len(receiver_data)/packet_count)
             receiver_check_sum = rcs.gen_CheckSum(receiver_data, packet_length)
-            connection.sendall(str.encode(receiver_check_sum))
+            if int(receiver_check_sum) == 0:
+                receiver_response = "\nError Checking Complete\nNo Error found in Receiver-data"
+            else:
+                receiver_response = "\nError Checking Complete\nError found in Receiver-data"
+            connection.sendall(str.encode(receiver_response))
 
         elif choice == '4':
-            key = input("Enter key for calculating CRC \n(Must be same as Sender's Key to get authentic results) : ")
+            # key = input("\nEnter key for calculating CRC \n(Must be same as Sender's Key to get authentic results) : ")
+            key = "10110"
             crc_data = crc.gen_CRC(receiver_data, key)
-            Receiver_response = crc_data
-            connection.sendall(str.encode(Receiver_response))
-    # connection.close()
+            if int(crc_data) == 0:
+                receiver_response = "\nError Checking Complete\nNo Error found in Receiver-data"
+            else:
+                receiver_response = "\nError Checking Complete\nError found in Receiver-data"
+            connection.sendall(str.encode(receiver_response))
 
 
 def main():
