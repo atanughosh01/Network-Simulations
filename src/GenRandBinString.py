0e0a0e1ce8c4ec3fa0923a3e55894e9c951f5740
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2021-08-07 14:00:36 +0530

    modified src/GenRandBinString.py

--- src/GenRandBinString.py
+++ src/GenRandBinString.py
@@ -1,25 +1,35 @@
 # Generates random binary string of given size
 
-import random
 import sys
+import random
 
 
-def gen_rand_string(size):
+def gen_rand_string(size: int) -> str:
     string = ""
     for i in range(size):
         temp = str(random.randint(0, 1))
         string += temp
     return string
 
-try:
-    # enter an integer of multiple of 8 for understanable string generation
+
+def main():
+    # enter a positive integer for string generation
     n = int(input("Enter length of the string to be generated : "))
     bin_str = gen_rand_string(n)
-except:
-    print("ERROR! Only integer inputs are accepted")
-    sys.exit()
 
-with open("inputtxt.", "w") as text_file:
-    text_file.write(bin_str)
+    with open("input.txt", "w") as text_file:
+        text_file.write(bin_str)
 
-print("Random binary string of taken length is : " + bin_str)
+    count1, count2 = 0, 0
+    for i in bin_str:
+        if i == '1':
+            count1 += 1
+        else:
+            count2 += 1
+    print("\nLength of generated binary string = " + str(len(bin_str)))
+    print("No. of 1-s present = " + str(count1) + 
+          "\nNo. of 0-s present = " + str(count2))
+
+
+if __name__ == "__main__":
+    main()
