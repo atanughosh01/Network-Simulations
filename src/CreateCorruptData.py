b7753165ba2c8445dc2b13418af112fee953a8e2
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2021-08-07 14:02:03 +0530

    modified src/GenRand

--- src/CreateCorruptData.py
+++ src/CreateCorruptData.py
@@ -1,21 +1,31 @@
+
 import random
 import GenRandError as gre
 
 with open("input.txt", "r") as file:
-    info = file.read()
-string_length = len(info)
-packet_count = 20
+    data = file.read()
+
+print("DATA = " + str(data))
+string_length = len(data)
+packet_count = 10
 packet_length = int(string_length/packet_count)
+print("\nLength of data = " + str(string_length))
+print("Number of packets = " + str(packet_count))
+print("Length of each packet = " + str(packet_length))
 
 packet_list = []
 for i in range(packet_count):
     k = packet_length
-    packet = info[i*k: (i+1)*k]
+    packet = data[i*k: (i+1)*k]
     packet_list.append(packet)
+
+print("\nOriginal Packet List :", packet_list)
+# for i in packet_list:
+#     print(len(i))
 
 corrupt_packet_list = []
 for packet in packet_list:
     corrupt_packet = gre.gen_rand_error(packet, random.randint(0, 8))
     corrupt_packet_list.append(corrupt_packet)
     
-print(corrupt_packet_list)
+print("\nCorrupted Packet List :", corrupt_packet_list)
