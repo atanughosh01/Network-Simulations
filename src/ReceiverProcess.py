18c683b3f93ec5d884787089c3049875f611d349
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2021-08-10 17:07:17 +0530

    added src/ReceiverProcess.py

--- src/ReceiverProcess.py
+++ src/ReceiverProcess.py
@@ -12,6 +12,7 @@ def receive_data():
     port = 65432                                # arbitrary non-privileged port
     thread_count = 0
     receiver_side_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    receiver_side_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
     try:
         receiver_side_socket.bind((host, port))
@@ -30,7 +31,7 @@ def receive_data():
         sender_connection, address = receiver_side_socket.accept()
         ip, port = str(address[0]), str(address[1])
         print("\nConnected to sender via address : " + ip + ':' + port)
-        start_new_thread(channel_thread, (sender_connection, ))
+        start_new_thread(sender_thread, (sender_connection, ))
         thread_count += 1
         print("Thread number = Sender number = " + str(thread_count))
 
@@ -38,36 +39,37 @@ def receive_data():
     print("Receiver has been terminated. Socket has been closed.")
 
 
-def channel_thread(connection: socket.socket):
+def sender_thread(connection: socket.socket):
     while True:
         sender_request = connection.recv(2048)
         receiver_data, choice = [str(i) for i in sender_request.decode("utf-8").split("\n")]
 
         if choice == '1':
             vrc_data = vrc.gen_VRC(receiver_data)
-            if int(vrc_data) == 0: receiver_response = "\nNO ERROR HAS BEEN FOUND"
-            else: receiver_response = "\nERROR HAS BEEN FOUND"
+            if int(vrc_data) == 0: receiver_response = "NO ERROR HAS BEEN FOUND"
+            else: receiver_response = "ERROR HAS BEEN FOUND"
             connection.sendall(str.encode(receiver_response))
 
         elif choice == '2':
             lrc_data = lrc.gen_LRC(receiver_data)
-            if int(lrc_data) == 0: receiver_response = "\nNO ERROR HAS BEEN FOUND"
-            else: receiver_response = "\nERROR HAS BEEN FOUND"
+            if int(lrc_data) == 0: receiver_response = "NO ERROR HAS BEEN FOUND"
+            else: receiver_response = "ERROR HAS BEEN FOUND"
             connection.sendall(str.encode(receiver_response))
 
         elif choice == '3':
             packet_count = 5
             packet_length = int(len(receiver_data)/packet_count)
             receiver_checksum = rcs.gen_CheckSum(receiver_data, packet_length)
-            if int(receiver_checksum) == 0: receiver_response = "\nNO ERROR HAS BEEN FOUND"
-            else: receiver_response = "\nERROR HAS BEEN FOUND"
+            if int(receiver_checksum) == 0: receiver_response = "NO ERROR HAS BEEN FOUND"
+            else: receiver_response = "ERROR HAS BEEN FOUND"
             connection.sendall(str.encode(receiver_response))
 
         elif choice == '4':
-            key = "10110"
+            # key = "111010101"                                       # CRC-8 (x^8 + x^7 + x^6 + x^4 + x^2 + 1)
+            key = "11000000000000101"                               # CRC-16 (x^16 + x^15 + x^2 + 1)
             crc_data = crc.gen_CRC(receiver_data, key)
-            if int(crc_data) == 0: receiver_response = "\nNO ERROR HAS BEEN FOUND"
-            else: receiver_response = "\nERROR HAS BEEN FOUND"
+            if int(crc_data) == 0: receiver_response = "NO ERROR HAS BEEN FOUND"
+            else: receiver_response = "ERROR HAS BEEN FOUND"
             connection.sendall(str.encode(receiver_response))
 
 
