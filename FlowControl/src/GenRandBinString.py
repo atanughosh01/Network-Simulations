95441221937418b4d6073e22ce53bd0f1403fc12
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-09-05 02:40:05 +0530

    added GenRandBinString.py

---
+++ FlowControl/src/GenRandBinString.py
@@ -0,0 +1,40 @@
+# Generates random binary string of given size
+import random
+
+
+class GenerateRandomBinaryString:
+
+    def __init__(self) -> None:
+        pass
+
+    def gen_rand_string(self, size: int) -> str:
+        string = ""
+        for i in range(size):
+            temp = str(random.randint(0, 1))
+            string += temp
+        return string
+
+
+def main():
+    # enter a positive integer for string generation
+    n = int(input("Enter length of the string to be generated : "))
+    grbs = GenerateRandomBinaryString()
+    bin_str = grbs.gen_rand_string(n)
+
+    try:
+        with open("textfiles/input.txt", "w") as text_file:
+            text_file.write(bin_str)
+    except FileNotFoundError as fnfe: print(str(fnfe))
+
+    count1, count2 = 0, 0
+    for i in bin_str:
+        if i == '1': count1 += 1
+        else: count2 += 1
+        
+    print("\nLength of generated binary string = " + str(len(bin_str)))
+    print("No. of 1-s present = " + str(count1) + 
+          "\nNo. of 0-s present = " + str(count2))
+
+
+if __name__ == "__main__":
+    main()
