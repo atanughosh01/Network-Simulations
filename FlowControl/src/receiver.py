8348553fabb212d205a519eb7837b7f06da796b2
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-09-05 02:40:46 +0530

    added receiver.py

--- FlowControl/src/receiver.py
+++ FlowControl/src/receiver.py
@@ -1,6 +1,15 @@
+import packages.go_back_N as go_back_N
+import packages.stop_and_wait as stop_and_wait
+import packages.selective_repeat as selective_repeat
 
-def recv():
-    pass
 
-def send_ack():
-    pass
+class Receiver:
+
+    def __init__(self) -> None:
+        pass
+
+    def recv():
+        pass
+
+    def send_ack():
+        pass
