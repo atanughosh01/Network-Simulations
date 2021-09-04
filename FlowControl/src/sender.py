757b571e473f7a33aa2147aad063c120b0b12f01
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-09-05 02:40:19 +0530

    added sender.py

--- FlowControl/src/sender.py
+++ FlowControl/src/sender.py
@@ -1,9 +1,18 @@
+import packages.go_back_N as go_back_N
+import packages.stop_and_wait as stop_and_wait
+import packages.selective_repeat as selective_repeat
 
-def send():
-    pass
 
-def recv_ack():
-    pass
+class Sender:
 
-def timeout():
-    pass
+    def __init__(self) -> None:
+        pass
+
+    def send():
+        pass
+
+    def recv_ack():
+        pass
+
+    def timeout():
+        pass
