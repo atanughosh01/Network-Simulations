35b90b218885b8893418619228c015cac337c8c6
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-09-05 02:40:31 +0530

    added channel.py

--- FlowControl/src/channel.py
+++ FlowControl/src/channel.py
@@ -1,3 +1,21 @@
+import packages.go_back_N as go_back_N
+import packages.stop_and_wait as stop_and_wait
+import packages.selective_repeat as selective_repeat
 
-def channel_process():
-    pass
+
+class Channel:
+
+    def __init__(self) -> None:
+        pass
+
+    def inject_errpr(self, packet):
+        pass
+
+    def sender_packet(self, sender):
+        pass
+
+    def receiver_ack(self, receiver):
+        pass
+
+    def channel_process():
+        pass
