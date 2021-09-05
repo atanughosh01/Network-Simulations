84665e3e8d99a70aea5f29b6b6ea195594d5483a
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-09-05 20:09:14 +0530

    modified receiver.py

--- FlowControl/src/receiver.py
+++ FlowControl/src/receiver.py
@@ -1,3 +1,8 @@
+import sys
+import socket
+import random
+import threading
+from _thread import start_new_thread
 import packages.go_back_N as go_back_N
 import packages.stop_and_wait as stop_and_wait
 import packages.selective_repeat as selective_repeat
