5b62bff558c7c7b0b3f59743f6a47e08b82a3c56
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-09-13 03:20:02 +0530

    modified selectiverepeat/channel.py

--- FlowControl/src/Final/selectiverepeat/channel.py
+++ FlowControl/src/Final/selectiverepeat/channel.py
@@ -1,8 +1,6 @@
 import socket
 import time
-import subprocess
 import random
-import os 
 
 def injectRandomError(frame):
 	pos = random.randint(0, len(frame)-1)
@@ -56,13 +54,12 @@ class Channel():
 		self.windowsize = windowsize
 		self.slidingwindow = []
 		self.currentcount = 0
-		#self.statuswindow = []
 		
 	def initSenders(self):
 		senderSocket = socket.socket()
 		senderSocket.bind((self.senderhost, self.senderport))
 		senderSocket.listen(self.totalsender)
-		for i in range(1, self.totalsender+1):
+		for _ in range(1, self.totalsender+1):
 			conn = senderSocket.accept()
 			self.senderconn.append(conn)
 		print('Initiated all sender connections')
@@ -76,7 +73,7 @@ class Channel():
 		receiverSocket = socket.socket()
 		receiverSocket.bind((self.receiverhost, self.receiverport))
 		receiverSocket.listen(self.totalreceiver)
-		for i in range(1, self.totalreceiver+1):
+		for _ in range(1, self.totalreceiver+1):
 			conn = receiverSocket.accept()
 			self.receiverconn.append(conn)
 		print('Initiated all receiver connections')
@@ -87,25 +84,18 @@ class Channel():
 		print('Closed all receiver connections')
 			
 	def processData(self):
-		'''fileout = open('flag.txt', "w")
-		fileout.write(str(0))
-		fileout.close()'''
 		while True:
 			for i in range(len(self.senderconn)):
 				print()
-				
 				conn = self.senderconn[i]
 				data = conn[0].recv(1024).decode()
 				prevtime = time.time()
 				data = str(data)
 				origmsg = extractMessage(data)
-				if not origmsg:
-					break
-				if origmsg == 'q0':
-					break
+				if not origmsg: break
+				if origmsg == 'q0': break
 				print('Received from Sender',i+1,':',str(data))
-				
-				
+
 				recvno = random.randint(0,len(self.receiverconn)-1) 
 				print('Sending to Receiver',recvno+1)
 				rconn = self.receiverconn[recvno]
@@ -119,15 +109,9 @@ class Channel():
 				rdata = str(rdata)
 				time.sleep(0.5)
 				curtime = time.time()
-				if curtime-prevtime > 2:
-					timeout = 1
-					newdata += 'TIMEOUT'
-				else:
-					timeout = 0
-					newdata += rdata
-				
+				if curtime-prevtime > 2: newdata += 'TIMEOUT'
+				else: newdata += rdata
 				self.slidingwindow.append([data, newdata, i, recvno])
-				
 				
 				msg = extractMessage(newdata)
 				cnt = extractCount(newdata)
@@ -138,7 +122,6 @@ class Channel():
 				if (self.currentcount % windowsize)+1 == self.windowsize:
 					idx = 0
 					flag = 1
-					
 					while flag == 1:
 						idx = 0
 						flag = 0
@@ -154,20 +137,15 @@ class Channel():
 								flag = 1
 								nakframes.append(self.slidingwindow[idx])
 								indices.append(idx+1)
-								#break
 							idx += 1
 						
 						if flag==0:
 							print(' ------------------------------ ')	
 							print('BLOCK OF WINDOW SIZE',self.windowsize,'SUCCESSFULLY SENT')
-							print(' ------------------------------ ')	
-						'''fileout = open('flag.txt', "w")
-						fileout.write(str(flag))
-						fileout.close()'''
+							print(' ------------------------------ ')
 						idx = 0
 						
 						while flag == 1 and idx < len(nakframes):
-							
 							print()
 							prevtime = time.time()
 							prevframe = nakframes[idx][0]
@@ -181,10 +159,7 @@ class Channel():
 							print(' ------------------------------ ')
 							print('RESENDING FRAME NO:',str(indices[idx]))
 							print(' ------------------------------ ')	
-							#sending all frames to its sender from first NAK 							
-							#conn[0].send(currframe.encode())
-							
-							#data = conn[0].recv(1024).decode()
+							#sending all frames to its sender from first NAK
 							print('Current frame no:',str(indices[idx]))
 							print('Again Sending to Receiver',recvno+1)
 							
@@ -206,31 +181,16 @@ class Channel():
 							print('Round trip time: ',str(curtime-prevtime))
 							self.slidingwindow[indices[idx]-1][1] = data 
 							idx += 1
-										
-							
-						'''fileout = open('flag.txt', "w")
-						fileout.write(str(0))
-						fileout.close()	'''
-						
-				
+
 				self.currentcount += 1
 			if origmsg == 'q0':
 				break
 		return
 
 if __name__ == '__main__':
-	#Main()
-	'''totalsen = 2
-	print('Number of senders',totalsen)
-	totalrecv = 2
-	print('Number of receivers',totalrecv)
-	windowsize = 4
-	print('Window size:',windowsize)
-	'''
 	totalsen = int(input('Enter number of senders: '))
 	totalrecv = int(input('Enter number of receivers: '))
 	windowsize =int(input('Enter window size: '))
-	
 	
 	ch = Channel(totalsen, totalrecv, windowsize)
 	ch.initSenders()
