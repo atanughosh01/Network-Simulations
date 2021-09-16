fe14f3f9d97a741d57c62ad17ba73d498e22646e
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-09-17 04:08:15 +0530

    created and modified source code structure

---
+++ CSMA/src/main.py
@@ -0,0 +1,9 @@
+segment_data = [12, 234, 451, 90, 7]
+
+for i in range(len(segment_data)):
+    print(segment_data[i])
+
+print("\n#####################\n")
+
+for tup in enumerate(segment_data):
+    print(tup[0], tup[1])
