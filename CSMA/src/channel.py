1c5bc6d086ab4bd406363b6e32ed1deb57cb5210
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-09-19 00:44:45 +0530

    added channel.py

--- CSMA/src/channel.py
+++ CSMA/src/channel.py
@@ -1,218 +1,69 @@
+import sys
 import time
+import const
 import socket
-import random
-
-
-def inject_random_error(frame):
-    pos = random.randint(0, len(frame)-1)
-    frame = frame[:pos]+'1'+frame[pos+1:]
-    return frame
-
-
-def extract_message(frame):
-    endidx = -1
-    for i in range(len(frame)-1):
-        if frame[i] == '/' and endidx == -1:
-            endidx = i
-            break
-    return frame[:endidx]
-
-
-def extract_count(frame):
-    startidx = -1
-    endidx = -1
-    for i in range(len(frame)-1):
-        if frame[i] == '/':
-            if startidx == -1:
-                startidx = i+1
-            else:
-                endidx = i
-    cnt = frame[startidx:endidx]
-    return int(cnt)
-
-
-def extract_status(frame):
-    count = 0
-    startidx = -1
-    for i in range(len(frame)-1):
-        if frame[i] == '/':
-            count += 1
-        if count == 2 and startidx == -1:
-            startidx = i+1
-            break
-    return frame[startidx:]
+import threading
+from gen_packet import *
+# from package import gen_packet as GP
 
 
 class Channel():
 
-    def __init__(self, totalsender, totalreceiver, windowsize):
-        self.totalsender = totalsender
-        self.senderhost = '127.0.0.1'
-        self.senderport = 8080
-        self.senderconn = []
+    # def __init__(self, sender_to_channel: socket.socket, channel_to_sender: list, receiver_to_channel: list, channel_to_receiver: list):
+    def __init__(self, sender_to_channel, channel_to_sender: list, receiver_to_channel: list, channel_to_receiver: list):
+        # self.senderhost = '127.0.0.1'
+        # self.senderport = 8080
+        # self.senderconn = []
+        # self.receiverhost = '127.0.0.2'
+        # self.receiverport = 9090
+        # self.receiverconn = []
 
-        self.totalreceiver = totalreceiver
-        self.receiverhost = '127.0.0.2'
-        self.receiverport = 9090
-        self.receiverconn = []
+        self.is_active = False
+        self.sender_to_channel = sender_to_channel
+        self.channel_to_sender = channel_to_sender
+        self.receiver_to_channel = receiver_to_channel
+        self.channel_to_receiver = channel_to_receiver
 
-        self.windowsize = windowsize
-        self.slidingwindow = []
-        self.currentcount = 0
-
-    def initialize_senders(self):
-        sender_side_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sender_side_socket.setsockopt(
-            socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sender_side_socket.bind((self.senderhost, self.senderport))
-        sender_side_socket.listen(self.totalsender)
-        for _ in range(1, self.totalsender+1):
-            conn = sender_side_socket.accept()
-            self.senderconn.append(conn)
-        print('Initiated all sender connections')
-
-    def terminate_senders(self):
-        for conn in self.senderconn:
-            conn[0].close()
-        print('Closed all sender connections')
-
-    def initialize_receivers(self):
-        receiver_side_socket = socket.socket(
-            socket.AF_INET, socket.SOCK_STREAM)
-        receiver_side_socket.setsockopt(
-            socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        receiver_side_socket.bind((self.receiverhost, self.receiverport))
-        receiver_side_socket.listen(self.totalreceiver)
-        for _ in range(1, self.totalreceiver+1):
-            conn = receiver_side_socket.accept()
-            self.receiverconn.append(conn)
-        print('Initiated all receiver connections')
-
-    def terminate_receivers(self):
-        for conn in self.receiverconn:
-            conn[0].close()
-        print('Closed all receiver connections')
-
-    def process_data(self):
+    def transfer_pkt_from_sender_to_receiver(self):
         while True:
-            for i in range(len(self.senderconn)):
-                print()
-                conn = self.senderconn[i]
-                data = conn[0].recv(1024).decode('utf-8')
-                prevtime = time.time()
-                data = str(data)
-                origmsg = extract_message(data)
-                if not origmsg:
-                    break
-                if origmsg == 'q0':
-                    break
-                print('Received from Sender', i+1, ':', str(data))
+            pkt = self.sender_to_channel.recv()
+            self.is_active = True
+            time.sleep(const.channel_propagation_time)
+            self.is_active = False
+            receiver = pkt.decode_dest_address()
+            self.channel_to_receiver[receiver].send(pkt)
 
-                recvno = random.randint(0, len(self.receiverconn)-1)
-                print('Sending to Receiver', recvno+1)
-                rconn = self.receiverconn[recvno]
-                cnt = extract_count(data)
-                msg = inject_random_error(origmsg)
-                newdata = msg + '/' + str(cnt) + '/'
-                rconn[0].sendto(newdata.encode(), rconn[1])
+    def tarnsfer_response_from_receiver_to_sender(self, sender: int):
+        while True:
+            if self.is_active:
+                self.channel_to_sender[sender].send(str(1))  # channel is busy
+            else:
+                self.channel_to_sender[sender].send(str(0))  # channel is idle
 
-                # received from receiver
-                rdata = rconn[0].recv(1024).decode()
-                rdata = str(rdata)
-                time.sleep(0.5)
-                curtime = time.time()
-                if curtime-prevtime > 2:
-                    newdata += 'TIMEOUT'
-                else:
-                    newdata += rdata
-                self.slidingwindow.append([data, newdata, i, recvno])
+    def initiate_channel_process(self):
+        print("\nCHANNEL has been initialised\n")
+        channel_to_receiver_thrdlst = []
+        channel_to_sender_thrdlst = []
+        sender = 0
 
-                msg = extract_message(newdata)
-                cnt = extract_count(newdata)
-                status = extract_status(newdata)
-                print(msg, str(cnt), status)
-                print('Round trip time: ', str(curtime-prevtime))
-                print('Current frame no:', str(
-                    (self.currentcount % windowsize)+1))
-                if (self.currentcount % windowsize)+1 == self.windowsize:
-                    idx = 0
-                    flag = 1
-                    while flag == 1:
-                        idx = 0
-                        flag = 0
-                        nakframes = []
-                        indices = []
-                        while idx < self.windowsize:
-                            currframe = self.slidingwindow[idx][1]
-                            msg = extract_message(currframe)
-                            cnt = extract_count(currframe)
-                            status = extract_status(currframe)
+        pkt_thrd = threading.Thread(name="PacketThread-"+str(sender+1),
+                                    target=self.transfer_pkt_from_sender_to_receiver)
+        channel_to_receiver_thrdlst.append(pkt_thrd)
 
-                            if status == 'NAK' or status == 'TIMEOUT':
-                                flag = 1
-                                nakframes.append(self.slidingwindow[idx])
-                                indices.append(idx+1)
-                            idx += 1
+        for _ in range(const.total_sender_number):
+            resp_thrd = threading.Thread(name="ResponseThread-"+str(sender+1),
+                                         target=self.tarnsfer_response_from_receiver_to_sender, args=(sender,))
+            channel_to_sender_thrdlst.append(resp_thrd)
+            sender += 1
 
-                        if flag == 0:
-                            print(' ------------------------------ ')
-                            print('BLOCK OF WINDOW SIZE',
-                                  self.windowsize, 'SUCCESSFULLY SENT')
-                            print(' ------------------------------ ')
+        for thread in channel_to_receiver_thrdlst:
+            thread.start()
 
-                        idx = 0
-                        while flag == 1 and idx < len(nakframes):
-                            print()
-                            prevtime = time.time()
-                            prevframe = nakframes[idx][0]
-                            currframe = nakframes[idx][1]
-                            sendno = nakframes[idx][2]
-                            recvno = nakframes[idx][3]
-                            conn = self.senderconn[sendno]
-                            rconn = self.receiverconn[recvno]
-                            stat = extract_status(currframe)
+        for thread in channel_to_sender_thrdlst:
+            thread.start()
 
-                            print(' ------------------------------ ')
-                            print('RESENDING FRAME NO:', str(indices[idx]))
-                            print(' ------------------------------ ')
+        for thread in channel_to_receiver_thrdlst:
+            thread.join()
 
-                            # sending all frames to its sender from first NAK
-                            print('Current frame no:', str(indices[idx]))
-                            print('Again Sending to Receiver', recvno+1)
-
-                            msg = extract_message(prevframe)
-                            msg = inject_random_error(msg)
-                            data = msg + '/' + str(cnt) + '/'
-                            rconn[0].sendto(data.encode(), rconn[1])
-
-                            # receiving ACK or NAK from receiver
-                            rdata = rconn[0].recv(1024).decode()
-                            rdata = str(rdata)
-                            data += rdata
-
-                            msg = extract_message(data)
-                            cnt = extract_count(data)
-                            stat = extract_status(data)
-                            curtime = time.time()
-                            print(msg, str(cnt), stat)
-                            print('Round trip time: ', str(curtime-prevtime))
-                            self.slidingwindow[indices[idx]-1][1] = data
-                            idx += 1
-
-                self.currentcount += 1
-            if origmsg == 'q0':
-                break
-        return
-
-
-if __name__ == '__main__':
-    totalsen = int(input('Enter number of senders: '))
-    totalrecv = int(input('Enter number of receivers: '))
-    windowsize = int(input('Enter window size: '))
-
-    ch = Channel(totalsen, totalrecv, windowsize)
-    ch.initialize_senders()
-    ch.initialize_receivers()
-    ch.process_data()
-    ch.terminate_senders()
-    ch.terminate_receivers()
+        for thread in channel_to_sender_thrdlst:
+            thread.join()
