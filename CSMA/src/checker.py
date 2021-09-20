c88dd967f35969b61aa3735c562d333140fc3981
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-09-20 10:09:38 +0530

    modified CSMA/src/checker.py

--- CSMA/src/checker.py
+++ CSMA/src/checker.py
@@ -5,22 +5,30 @@ def check_sum(seg_data: str) -> str:
     '''returns 32 bit checksum of given input'''
     total = 0
     data = [seg_data[i: i+32] for i in range(0, len(seg_data), 32)]
-    for i in data:
-        total += int(i, 2)
+    for j in data:
+        total += int(j, 2)
         if total >= 4294967295:
-            total -= 4294967295
-
+            total -= 4294967295     # 2^32 - 1 = 4294967295
     cksum = 4294967295 - total
     cksum_bits = '{0:032b}'.format(cksum)
     return cksum_bits
 
 
 def check_error(seg_data: str) -> bool:
-    '''returns true if errors present else false'''
+    '''returns True if errors are not present else False'''
     total = 0
     data = [seg_data[i: i+32] for i in range(0, len(seg_data), 32)]
-    for i in data:
-        total += int(i, 2)
+    for j in data:
+        total += int(j, 2)
         if total >= 4294967295:
-            total -= 4294967295
+            total -= 4294967295     # 2^32 - 1 = 4294967295
     return 1 if total == 0 else 0
+
+
+# segment_data = [12, 234, 451, 90, 7]
+# if __name__ == "__main__":
+#     for i in range(len(segment_data)):
+#         print(segment_data[i])
+#     print("\n---------------------------\n")
+#     for tup in enumerate(segment_data):
+#         print(tup[0], '\t', tup[1])
