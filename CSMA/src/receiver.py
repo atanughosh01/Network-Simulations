2cd97f3c0dc7b1b377406fffea709acedd36ffa5
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-09-20 10:11:04 +0530

    modified CSMA/src/receiver.py

--- CSMA/src/receiver.py
+++ CSMA/src/receiver.py
@@ -1,12 +1,15 @@
+'''Receiver Module for packet reception'''
+
 import sys
 import const
 from gen_packet import Packet
 
 
 class Receiver:
+    '''Receiver Class to implement packet receiving functionalities'''
 
     def __init__(self, name: int, channel_to_receiver):
-        self.seq_no = 0
+        self.seq_no = 0             # need to be synced with sender
         self.name = name
         self.sender_list = {}
         self.packet_type = {'data': 0, 'ack': 1}
@@ -14,6 +17,7 @@ class Receiver:
         self.recent_ack = Packet(1, 0, "Acknowledgement Packet", self.name, 0).make_pkt()
 
     def open_file(self, filepath: str):
+        '''Opens file in append mode and returns file-pointer-object'''
         try: fptr = open(filepath, 'a+', encoding='utf-8')
         except FileNotFoundError as file_err:
             print("\nEXCEPTION Caught : " + str(file_err))
@@ -21,14 +25,16 @@ class Receiver:
         return fptr
 
     def decode_sender(self, pkt):
+        '''Decodes source-address from the received packets'''
         sender_address = pkt.decode_src_address()
         return sender_address
 
     def initiate_receiver_process(self):
+        '''Receives & decodes packets from sender, extracts data and writes it to output file'''
         while True:
             pkt = self.channel_to_receiver.recv()
             sender = self.decode_sender(pkt)
-            
+
             if sender not in self.sender_list.keys():
                 self.sender_list[sender] = const.outfile_path + 'output' + str(sender)
 
