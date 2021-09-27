8e0327126259ef2822b296b07b97e2aa037b3518
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-09-28 00:14:22 +0530

    added receiver.py

--- CDMA/src/receiver.py
+++ CDMA/src/receiver.py
@@ -1,6 +1,7 @@
 import sys
 import const
 import threading
+from datetime import datetime
 
 # curr_datetime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 class Receiver:
@@ -15,58 +16,69 @@ class Receiver:
 
     def select_sender(self):
         return self.name
-  
+
 
     def get_char(self, data):
-        print("Data:" + str(data))
+        # print("Data:" + str(data))
+        curr_datetime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        print("{} ||| DATA : {}".format(curr_datetime, str(data)))
+        with open('textfiles/report.txt', 'a+', encoding='utf-8') as rep_file:
+            rep_file.write("\n{} ||| DATA : {}".format(curr_datetime, str(data)))
         summation = 0
         for i in range(8): summation += pow(2,i) * data[7-i]
         character = chr(summation)
-        print("Char received: " + character)
+        curr_datetime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        print("\n{} ||| CHAR RECEIVED : {}".format(curr_datetime, character))
+        with open('textfiles/report.txt', 'a+', encoding='utf-8') as rep_file:
+            rep_file.write("\n\n{} ||| CHAR RECEIVED : {}\n".format(curr_datetime, character))
         return character
-    
+
 
     def open_file(self, sender):
         try:
             file_name = const.output_file_path + 'output' + str(sender+1) + '.txt'
             fptr = open(file_name, 'a+', encoding='utf-8')
         except FileNotFoundError as fnfe:
-            print("EXCEPTION CAUGHT : {}".format(str(fnfe)))
+            curr_datetime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            print("{} EXCEPTION CAUGHT : {}".format(curr_datetime, str(fnfe)))
             sys.exit("No file exists with name {} !".format(file_name))
         return fptr
-    
+
 
     def receive_data(self):
 
-        print("(Receiver{}:) Receiver{} receives data from sender{}".format(self.name+1, self.name+1, self.sender_to_receiver+1))
+        curr_datetime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        print("{} ||| RECEIVER-{}   ||  RECEIVES DATA FROM SENDER-{}".format(curr_datetime, self.name+1, self.sender_to_receiver+1))
+        with open('textfiles/report.txt', 'a+', encoding='utf-8') as rep_file:
+            rep_file.write("\n{} ||| RECEIVER-{}   ||  RECEIVES DATA FROM SENDER-{}".format(curr_datetime, self.name+1, self.sender_to_receiver+1))
         total_data = []
         while True:
             channel_data = self.channel_to_receiver.recv()
 
             # extract data
             summation = 0
-            for i in range(len(channel_data)):
-                summation += channel_data[i] * self.wls_table[self.sender_to_receiver][i]
-            
+            for i in range(len(channel_data)): summation += channel_data[i] * self.wls_table[self.sender_to_receiver][i]
+
             # extract data bit
             summation /= self.code_length
             if summation == 1: bit = 1
             elif summation == -1: bit = 0
             else: bit = -1
-            
-            print("(Receiver{}:) Bit received: {}".format(self.name+1, bit))
 
-            if len(total_data) < 8 and bit != -1:
-                #print("Receiver{} -->> Lenght of total_data {}".format(self.name+1, len(total_data)))
-                total_data.append(bit)
+            curr_datetime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            print("{} ||| RECEIVER-{}   ||  BIT RECEIVED : {}".format(curr_datetime, self.name+1, bit))
+            with open('textfiles/report.txt', 'a+', encoding='utf-8') as rep_file:
+                rep_file.write("\n{} ||| RECEIVER-{}   ||  BIT RECEIVED : {}".format(curr_datetime, self.name+1, bit))
 
-            if(len(total_data) == 8):
+            if len(total_data) < 8 and bit != -1: total_data.append(bit)
+
+            if len(total_data) == 8:
                 character = self.get_char(total_data)
                 output_file = self.open_file(self.sender_to_receiver)
                 output_file.write(character)
                 output_file.close()
                 total_data = []
-                
+
 
     def start_receiver(self):
         receiver_thread = threading.Thread(name='Receiver-Thread', target=self.receive_data)
