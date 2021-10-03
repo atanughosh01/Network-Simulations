7494a2c0a12d3c5c1f3f68a72cf3b14bfbba5035
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-10-03 15:24:03 +0530

    simulated the project and added textfiles

--- CDMA/src/const.py
+++ CDMA/src/const.py
@@ -2,11 +2,11 @@
 
 import random
 
-total_sender_number       = 2
-total_receiver_number     = 2
+total_sender_number       = 8
+total_receiver_number     = 8
 default_data_packet_size  = 1
 
 sender_sleep_time         = random.uniform(0.3, 0.8)
 
-input_file_path           = "./textfiles/input15/"
+input_file_path           = "./textfiles/input/"
 output_file_path          = "./textfiles/output/"
