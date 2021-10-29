0612a8d052ecf0d6a0fed161b3f7724d53170276
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-10-29 10:22:46 +0530

    Directory Structure Modified

--- 4_CDMA/src/walsh.py
+++ 4_CDMA/src/walsh.py
@@ -1,11 +1,13 @@
+'''Walsh CEncoding Function'''
 
-def get_next_powerof2(n):
+def get_next_powerof2(num):
     power = 1
-    while(power < n): power *= 2
+    while power < num:
+        power *= 2
     return power
 
 
-def build_walsh_table(length, i1,i2, j1,j2, is_complement):
+def build_walsh_table(length, i1, i2, j1, j2, is_complement):
     if length == 2:
         if not is_complement:
             wls_table[i1][j1] = 1
@@ -17,9 +19,9 @@ def build_walsh_table(length, i1,i2, j1,j2, is_complement):
             wls_table[i1][j2] = -1
             wls_table[i2][j1] = -1
             wls_table[i2][j2] = 1
-        
+
         return
-    
+
     mid_i = (i1+i2)//2
     mid_j = (j1+j2)//2
 
@@ -33,10 +35,10 @@ def build_walsh_table(length, i1,i2, j1,j2, is_complement):
 
 if __name__ == "__main__":
     num_of_stations = int(input("Enter Number of Stations : "))
-    num = get_next_powerof2(num_of_stations)
-    wls_table = [[0 for _ in range(num)] for _ in range(num)]
+    NUM = get_next_powerof2(num_of_stations)
+    wls_table = [[0 for _ in range(NUM)] for _ in range(NUM)]
 
-    build_walsh_table(num, 0, num - 1, 0, num - 1, False)
+    build_walsh_table(NUM, 0, NUM - 1, 0, NUM - 1, False)
     print(wls_table)
     print(len(wls_table[0]))
-    print(num)
+    print(NUM)
