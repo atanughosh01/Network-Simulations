a7f1e3cab8ee50ff77ffc1b3b0071bee7fb01d13
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2021-11-03 02:52:16 +0530

    modified ASG2

--- 2_FlowControl/src/Trial/channel.py
+++ 2_FlowControl/src/Trial/channel.py
@@ -1,127 +1,96 @@
-import os
-import sys
-import time
-import socket
-import random
+import multiprocessing
 import threading
-import packages.const as const
-from template import *
-from _thread import start_new_thread
-import packages.go_back_N as go_back_N
-import packages.stop_and_wait as stop_and_wait
-import packages.selective_repeat as selective_repeat
-
+import random
+import time
+import helper
+from createPacket import *
+import const
 
 class Channel:
+    def __init__(self, senderToChannel, channelToSender, receiverToChannel, channelToReceiver):  
+        
+        self.senderToChannel    = senderToChannel
+        self.channelToSender    = channelToSender 
+        self.receiverToChannel  = receiverToChannel
+        self.channelToReceiver  = channelToReceiver
 
-    def __init__(self) -> None:
-        self.port = 65432
-        self.host = '127.0.0.1'
-        self.thread_count = 0
-        self.channel_side_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.channel_side_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    def injectError(self, packet):
+        noOfErrors = random.randint(const.minError, const.maxError)
+        listOFChar = list(packet.packet)
+        length = packet.decodeLength()
+        for i in range(noOfErrors):
+            pos = random.randint(0,length-1)
+            if listOFChar[pos] == '1': listOFChar[pos] = '0'
+            else: listOFChar[pos] = '1'
+            
+        packet.packet = ''.join(listOFChar)
 
-        try:
-            self.channel_side_socket.bind((self.host, self.port))
-            self.channel_side_socket.listen(5)
-            print("\nSocket has been created.\nChannel is waiting for client(s) to connect....")
-        except Exception as ex:
-            print("\n[ERROR 1] Error Description : " + str(sys.exc_info()))
-            print("[EXCEPTION 1] Exception : " + str(ex))
-            self.channel_side_socket.close()
-            print("\nReceiver has been terminated. Socket has been closed.")
-            sys.exit(1)
-
-
-    def inject_biterror(self, packet: str) -> str:
-        pkt_len = len(packet)
-        digit_list = list(packet)
-        index = random.randint(0, pkt_len-1)
-        if digit_list[index] == '1': digit_list[index] = '0'
-        elif digit_list[index] == '0': digit_list[index] = '1'
-        return ("".join(digit_list))
-
-
-    def send_packet_to_receiver(self, sender_conn: socket.socket):
+    def channelizePktFromSenderToReceiver(self,sender):
         time.sleep(0.5)
-        while True:
-            pkt = sender_conn.recv(1024).decode('utf-8')             # data-packet
-            receiver_conn, addr = sender_conn.accept()
-            if random.random() <= const.drop_out_prob:
-                print("CHANNEL : Packet has been discarded")
+        while True:    
+            packet = self.senderToChannel[sender].recv()
+            receiver = packet.decodeDestAddress()
+            if random.random() <= const.dropOutProb:
+                print("CHANNEL -->> PACKET DROPPED OUT") # returns a float between (0,1)
             else:
-                if random.random() <= const.err_inject_prob:
-                    print("CHANNEL : Injecting BIT-Error in Packet")
-                    self.inject_biterror(pkt)
+                if random.random() <= const.injectErrorProb:
+                    print("CHANNEL -->> INJECTING ERROR IN PACKET")
+                    self.injectError(packet)
 
-                if random.random() <= const.delay_prob: 
-                    print("CHANNEL : Introducing delay in Packet")                        
+                if random.random() <= const.delayProb: 
+                    print("CHANNEL -->> INTRODUCING DELAY IN PACKET")                        
                     time.sleep(const.delay)
                     
-                receiver_conn.send(pkt)
-                print("CHANNEL : Packet has been sent")
+                self.channelToReceiver[receiver].send(packet)
+                print("CHANNEL -->> PACKET SENT")
 
-
-    def send_ack_to_sender(self, receiver_conn: socket.socket):
+    def channelizeACKFromReceiverToSender(self, receiver): 
         time.sleep(0.5)
         while True:
-            ack = receiver_conn.recv(1024).decode('utf-8')            # acknowledgement
-            sender_conn, addr = receiver_conn.accept()
-            if random.random() <= const.drop_out_prob:
-                print("CHANNEL : Packet has been dropped out")
+            ack = self.receiverToChannel[receiver].recv()
+            sender = ack.decodeDestAddress()
+            if random.random() <= const.dropOutProb:
+                print("CHANNEL -->> ACK DROPPED OUT")
             else:
-                if random.random() <= const.err_inject_prob:
-                    print("CHANNEL : Injecting BIT-Error in Acknowledgement")
-                    self.inject_biterror(ack)
-
-                if random.random() <= const.delay_prob: 
-                    print("CHANNEL : Introducing delay in Acknowledgement")                        
+                if random.random() <= const.injectErrorProb:
+                    print("CHANNEL -->> INJECTING ERROR IN ACK")
+                    self.injectError(ack)
+                if random.random() <= const.delayProb:
+                    print("CHANNEL -->> INTRODUCING DELAY IN ACK")
                     time.sleep(const.delay)
     
-                sender_conn.send(ack)
-                print("CHANNEL : Acknowledgement has been sent")
+                self.channelToSender[sender].send(ack)
+                print("CHANNEL -->> ACK SENT")
 
+    def startChannel(self):
 
-    def initiate_channel_process(self):
-        sender_to_receiver_pkt_threadlist = []
-        receiver_to_sender_ack_thread_list = []
-        sndr_thrd_cnt = 0
-        recvr_thrd_cnt = 0
+        senderToReceiverThreadList = []
+        receiverToSenderThreadList = []
+        sender = 0
+        receiver = 0
         print("\nCHANNEL is running")
+        for i in range(const.totalSenderNumber):
+            t = threading.Thread(name= 'PktThread'+str(sender+1), 
+                                        target=self.channelizePktFromSenderToReceiver,
+                                            args=(sender,))
+            senderToReceiverThreadList.append(t)
+            sender += 1
+            
+        for i in range(const.totalReceiverNumber):
+            t = threading.Thread(name= 'ACKThread'+str(receiver+1), 
+                                        target=self.channelizeACKFromReceiverToSender, 
+                                            args=(receiver,))
+            receiverToSenderThreadList.append(t)
+            receiver += 1
 
-        while recvr_thrd_cnt <= const.total_receiver_number:
-            conn, addr = self.channel_side_socket.accept()
-            ip, port = str(addr[0]), str(addr[1])
-            with open("textfiles/recv_addr.txt", 'a') as recv_addr: recv_addr.write(str(addr) + "\n")
-            print("\nConnected to receiver via address : " + ip + ':' + port)
-            ackn_thrd = threading.Thread(name= 'AckmntThread-' + str(recvr_thrd_cnt+1), target=self.send_ack_to_sender, args=(conn,))
-            receiver_to_sender_ack_thread_list.append(ackn_thrd)
-            recvr_thrd_cnt += 1
-            print("Thread number = Receiver number = " + str(recvr_thrd_cnt))
-
-        while sndr_thrd_cnt <= const.total_sender_number:
-            conn, addr = self.channel_side_socket.accept()
-            ip, port = str(addr[0]), str(addr[1])
-            with open("textfiles/sndr_addr.txt", 'a') as sndr_addr: sndr_addr.write(str(addr) + "\n")
-            print("\nConnected to sender via address : " + ip + ':' + port)
-            pkt_thrd = threading.Thread(name= 'PacketThread-' + str(sndr_thrd_cnt+1), target=self.send_packet_to_receiver, args=(conn,))
-            sender_to_receiver_pkt_threadlist.append(pkt_thrd)
-            sndr_thrd_cnt += 1
-            print("Thread number = Sender number = " + str(sndr_thrd_cnt))
-
-        for thread in receiver_to_sender_ack_thread_list:
+        for thread in senderToReceiverThreadList:
+            thread.start()
+            
+        for thread in receiverToSenderThreadList:
             thread.start()
 
-        for thread in sender_to_receiver_pkt_threadlist:
-            thread.start()
-
-        for thread in sender_to_receiver_pkt_threadlist:
+        for thread in senderToReceiverThreadList:
             thread.join()
             
-        for thread in receiver_to_sender_ack_thread_list:
+        for thread in receiverToSenderThreadList:
             thread.join()
-
-
-if __name__ == "__main__":
-    chnl = Channel()
-    chnl.initiate_channel_process()
--- 2_FlowControl/src/Trial/receiver.py
+++ 2_FlowControl/src/Trial/receiver.py
@@ -1,64 +1,70 @@
-import os
-import sys
+import multiprocessing
 import time
-import socket
-import random
-import threading
-import packages.const as const
-from template import *
-from _thread import start_new_thread
-import packages.go_back_N as go_back_N
-import packages.stop_and_wait as stop_and_wait
-import packages.selective_repeat as selective_repeat
-
+import helper
+import const
+import sys
+from createPacket import *
 
 class Receiver:
+    def __init__(self, name, receiverToChannel, channelToReceiver):
+        self.name               = name
+        self.packetType         = {'data' : 0, 'ack' : 1}
+        self.senderList         = dict()
+        self.receiverToChannel  = receiverToChannel #write
+        self.channelToReceiver  = channelToReceiver #read
+        self.seqNo              = 0
+        self.recentACK          = Packet(1, 0, "Acknowledgement Packet", self.name, 0).makePacket()
 
-    def __init__(self) -> None:
-        self.port = 65432
-        self.host = '127.0.0.1'
-        self.recv_thread_count = 0
-        self.MAX = const.total_receiver_number
+    def sendAck(self, sender, seqNo):
+        packet = Packet(_type=self.packetType['ack'],
+                        seqNo=self.seqNo,
+                        segmentData='acknowledgement Packet',
+                        sender=self.name,
+                        dest=sender).makePacket()
+        
+        self.recentACK = packet
+        self.receiverToChannel.send(packet)
+    
+    def resendPreviousACK(self):
+        self.receiverToChannel.send(self.recentACK)
 
-    def gen_ack(self, pkt: str) -> str:
-        return "ack-" + pkt
+    def openFile(self, filepath):
+        try:
+            file = open(filepath, 'a+')
+        except IOError:
+            sys.exit("File path not exit!")
+        return file
 
-    def recv_pkt(self, conn: socket.socket):
-        pkt = conn.recv(1024).decode("utf-8")
-        return pkt
+    def decodeSeqNo(self, packet):
+        return packet.decodeSeqNo()
 
-    def send_ack(self, ack: str, conn: socket.socket):
-        ack = conn.sendall(ack)
-
-    def initiate_receiver_process(self):
-        self.receiver_side_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.receiver_side_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        try: self.receiver_side_socket.connect((self.host, self.port))
-        except socket.error as se:
-            print("\nChannel is currently inactive. Connection failed!\n\n[Error 1] : " + str(sys.exc_info()))
-            print("\n[EXCEPTION 1] SOCK_ERR Caught : " + str(se) + "\nConnection has been closed")
-            self.receiver_side_socket.close()
-            sys.exit(1)
-
-        print("\nChannel is currently active.\Waiting for sender to send request.")
-
-        self.sndr_req = self.receiver_side_socket.recv(2048)
-        recvd_pkt, choice = [str(i) for i in self.sndr_req.decode("utf-8").split("\n")]
-
-        if choice == '1':
-            ack_to_be_sent = self.gen_ack(recvd_pkt)
-            self.receiver_side_socket.sendall(ack_to_be_sent)
-
-
-        elif choice == '2':
-            ack_to_be_sent = self.gen_ack(recvd_pkt)
-            self.receiver_side_socket.sendall(ack_to_be_sent)
-
-        elif choice == '3':
-            ack_to_be_sent = self.gen_ack(recvd_pkt)
-            self.receiver_side_socket.sendall(ack_to_be_sent)
-
-
-if __name__ == "__main__":
-    recvr = Receiver()
-    recvr.initiate_receiver_process()
+    def decodeSender(self,packet):
+        senderAddress = packet.decodeSourceAddress()
+        return senderAddress
+  
+    def startReceiving(self):
+        time.sleep(0.4)
+        while True:
+            packet = self.channelToReceiver.recv()
+            print("RECEIVER{} -->> PACKET RECEIVED".format(self.name+1))
+            
+            if packet.checkForError():
+                print("RECEIVER{} -->> ERROR CHECKED".format(self.name+1))
+                sender = self.decodeSender(packet)
+                seqNo = self.decodeSeqNo(packet)
+                if self.seqNo == seqNo:
+                    if sender not in self.senderList.keys():
+                        self.senderList[sender] = const.outFilePath + 'output' + str(sender)
+                    outFile = self.senderList[sender]
+                    file = self.openFile(outFile)
+                    data = packet.extractData()
+                    file.write(data)
+                    file.close()
+                    self.seqNo = (self.seqNo+1) % const.windowSize
+                    self.sendAck(sender,self.seqNo)
+                    print("RECEIVER{} -->> ACK SENT FROM RECEIVER".format(self.name+1))
+                else:
+                    self.resendPreviousACK()
+                    print("RECEIVER{} -->> ACK RESENDED".format(self.name+1))
+            else:
+                print("RECEIVER{} -->> PACKET DISCARDED".format(self.name+1))
