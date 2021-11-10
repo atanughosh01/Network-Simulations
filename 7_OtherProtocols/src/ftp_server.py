ae26b1ba576d83e6f089c924a4c597a32daf2288
atanughosh01 <62764156+atanughosh01@users.noreply.github.com>
2021-11-10 21:24:38 +0530

    Add FTP Server Code

--- 7_OtherProtocols/src/ftp_server.py
+++ 7_OtherProtocols/src/ftp_server.py
@@ -1,3 +1,46 @@
-#!/usr/bin/env python3.9
+# !/usr/bin/env python3.9
 
 """FTP Server implementation in python"""
+
+import socket
+
+HOST = "127.0.0.1"
+FTP_PORT = 54200
+
+
+class FTPServer:
+    """FTP Server class"""
+
+    def __init__(self):
+        """Initialize FTP Server"""
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.name = ""
+        self.data = ""
+        self.file_name = ""
+
+    def start_ftp(self):
+        """Start The FTP Server"""
+
+        print("FTP Server started!!")
+        while True:
+            self.sock.bind((HOST, FTP_PORT))
+            print("Listening for a connection on its own port....")
+            self.sock.listen(5)
+            conn, addr = self.sock.accept()
+            self.name = conn.recv(1024).decode("utf-8")
+            self.file_name = conn.recv(1024).decode("utf-8")
+            print("{} with address {}'s requesting file {}".format(self.name, addr, self.file_name))
+            with open(self.file_name, "r", encoding="utf-8") as fptr:
+                self.data = fptr.read()
+            conn.send(bytes(self.data, "utf-8"))
+            print("File sent successfully")
+            conn.close()
+            self.sock.close()
+            print("FTP Server still running!")
+
+
+if __name__ == "__main__":
+    ftp_server = FTPServer()
+    ftp_server.start_ftp()
