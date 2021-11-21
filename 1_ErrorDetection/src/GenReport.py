52449e0a098160722a4a68d6f5abbc5e617e112c
Atanu Ghosh <62764156+atanughosh01@users.noreply.github.com>
2021-11-21 11:57:42 +0530

    Modified Directory Structure

--- 1_ErrorDetection/src/GenReport.py
+++ 1_ErrorDetection/src/GenReport.py
@@ -3,21 +3,22 @@ import sys
 
 while True:
     inp = input("\nEnter filename to generate report (Enter Q to QUIT) : ")
-    if inp == "Q" or "q": sys.exit("\n Quitting....\n QUITTED !!!\n")
-    try:
-        with open("textfiles/" + inp, "r") as text_file:
-            line_no, count1, count2 = 0, 0, 0
-            for line in text_file:
-                if line.startswith("NO "): count1 += 1
-                elif line.startswith("ERROR "): count2 += 1
-                line_no = count1 + count2
-                accuracy = (count2/line_no)*100
-                inaccuracy = (count1/line_no)*100
-        print("\nNumber of total errors to test on = " + str(line_no))
-        print("Number of errors skipped : " + str(count1))
-        print("Number of errors caught : " + str(count2))
-        print("Percentage accuracy of detecting error of this scheme = " + str(accuracy) + " %")
-        print("Percentage inaccuracy of detecting error of this scheme = " + str(inaccuracy) + " %\n")
-    except Exception as ex:
-        print("[EXCEPTION 1] " + str(ex))
-        continue
+    if inp in ("Q", "q"): sys.exit("\n Quitting....\n QUITTED !!!\n")
+    else:
+        try:
+            with open("textfiles/" + inp, "r") as text_file:
+                line_no, count1, count2 = 0, 0, 0
+                for line in text_file:
+                    if line.startswith("NO "): count1 += 1
+                    elif line.startswith("ERROR "): count2 += 1
+                    line_no = count1 + count2
+                    accuracy = (count2/line_no)*100
+                    inaccuracy = (count1/line_no)*100
+            print("\nNumber of total errors to test on = " + str(line_no))
+            print("Number of errors skipped : " + str(count1))
+            print("Number of errors caught : " + str(count2))
+            print("Percentage accuracy of detecting error of this scheme = " + str(accuracy) + " %")
+            print("Percentage inaccuracy of detecting error of this scheme = " + str(inaccuracy) + " %\n")
+        except Exception as ex:
+            print("[EXCEPTION 1] " + str(ex))
+            continue
